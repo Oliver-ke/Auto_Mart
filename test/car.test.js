3f1ce937de1accdaed66fb26d8b05ff08240a089
azorji kelechi oliver <kelechioliver96@gmail.com>
2019-05-24 07:15:58 +0100

    #166051932 created unsold car within price range api -feature

--- test/car.test.js
+++ test/car.test.js
@@ -290,7 +290,7 @@ describe('Car endpoint', () => {
       });
     });
   });
-  describe('api/v1/car?status=available', () => {
+  describe('api/v1/car?status=available || min_price&max_prce', () => {
     describe('GET', () => {
       it('Should get cars with status available', (done) => {
         chai.request(app).get('/api/v1/car?status=available').end((carErr, carRes) => {
@@ -306,6 +306,17 @@ describe('Car endpoint', () => {
           carRes.should.have.status(400);
           carRes.body.should.be.a('object');
           carRes.body.should.have.property('error');
+          done();
+        });
+      });
+      it('Should accept max_price and min_price query', (done) => {
+        chai.request(app).get('/api/v1/car?status=available&min_price=1500&max_price=20000').end((carErr, carRes) => {
+          carRes.should.have.status(200);
+          carRes.body.should.be.a('object');
+          carRes.body.data[0].price.should.be.above(1499);
+          carRes.body.data[0].price.should.be.below(20001);
+          carRes.body.should.have.property('data');
+          carRes.body.data.should.be.a('array');
           done();
         });
       });
