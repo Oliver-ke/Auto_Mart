71453558bf31eb7fdc5c84f9930e3748884ebf53
azorji kelechi oliver <kelechioliver96@gmail.com>
2019-06-18 01:21:15 +0100

    #166698768 Add API endp for admin to delete car in db

--- test/endpoints/car/deleteCar.test.js
+++ test/endpoints/car/deleteCar.test.js
@@ -1,54 +1,54 @@
-// /* global describe it */
-// import chai from 'chai';
-// import chaiHttp from 'chai-http';
-// import app from '../../../src/server';
+/* global describe it */
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../../../src/server';
 
-// chai.use(chaiHttp);
-// chai.should();
-// describe('DELETE', () => {
-//   it('Should accept delete request from admin', (done) => {
-//     // sign up a user to get auth token
-//     const user = {
-//       email: 'isAdmin@gmail.com',
-//       password: 'admin',
-//       first_name: 'Martins',
-//       last_name: 'John',
-//       address: 'GRA phase 2 Port harcourt',
-//       is_admin: true,
-//     };
-//     chai.request(app).post('/api/v1/auth/signup').send(user).end((err, res) => {
-//       chai
-//         .request(app)
-//         .delete('/api/v1/car/5')
-//         .set('authorization', `Bearer ${res.body.data.token}`)
-//         .end((carErr, carRes) => {
-//           carRes.should.have.status(200);
-//           carRes.body.should.be.a('object');
-//           done();
-//         });
-//     });
-//   });
-//   it('Should prevent delete request from none admin', (done) => {
-//     // sign up a user to get auth token
-//     const user = {
-//       email: 'smith@gmail.com',
-//       password: 'smith',
-//       first_name: 'Jades',
-//       last_name: 'Johnson',
-//       address: 'Rumuikurishi Port harcourt',
-//       is_admin: false,
-//     };
-//     chai.request(app).post('/api/v1/auth/signup').send(user).end((err, res) => {
-//       chai
-//         .request(app)
-//         .delete('/api/v1/car/5')
-//         .set('authorization', `Bearer ${res.body.data.token}`)
-//         .end((carErr, carRes) => {
-//           carRes.should.have.status(403);
-//           carRes.body.should.be.a('object');
-//           carRes.body.should.have.property('error');
-//           done();
-//         });
-//     });
-//   });
-// });
+chai.use(chaiHttp);
+chai.should();
+describe('DELETE', () => {
+	it('Should accept delete request from admin', (done) => {
+		// sign up a user to get auth token
+		const user = {
+			email: 'isAdmin@gmail.com',
+			password: 'admin',
+			first_name: 'Martins',
+			last_name: 'John',
+			address: 'GRA phase 2 Port harcourt',
+			is_admin: true
+		};
+		chai.request(app).post('/api/v1/auth/signup').send(user).end((err, res) => {
+			chai
+				.request(app)
+				.delete('/api/v1/car/3')
+				.set('authorization', `Bearer ${res.body.data.token}`)
+				.end((carErr, carRes) => {
+					carRes.should.have.status(200);
+					carRes.body.should.be.a('object');
+					done();
+				});
+		});
+	});
+	it('Should prevent delete request from none admin', (done) => {
+		// sign up a user to get auth token
+		const user = {
+			email: 'smith@gmail.com',
+			password: 'smith',
+			first_name: 'Jades',
+			last_name: 'Johnson',
+			address: 'Rumuikurishi Port harcourt',
+			is_admin: false
+		};
+		chai.request(app).post('/api/v1/auth/signup').send(user).end((err, res) => {
+			chai
+				.request(app)
+				.delete('/api/v1/car/5')
+				.set('authorization', `Bearer ${res.body.data.token}`)
+				.end((carErr, carRes) => {
+					carRes.should.have.status(403);
+					carRes.body.should.be.a('object');
+					carRes.body.should.have.property('error');
+					done();
+				});
+		});
+	});
+});
