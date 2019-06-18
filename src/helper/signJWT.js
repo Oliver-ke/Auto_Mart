1698c17c8df47b328a45451a4bcd76d6d86ee472
azorji kelechi oliver <kelechioliver96@gmail.com>
2019-06-18 09:26:24 +0100

    #166698785 Add admin view sold/unsold car endpoint with db

--- src/helper/signJWT.js
+++ src/helper/signJWT.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 export default async (userData) => {
   const payload = { id: userData.id, email: userData.email, isAdmin: userData.is_admin };
   try {
-    const token = await jwt.sign(payload, process.env.JWT_SECRET);
+    const token = await jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: 3600 });
     const user = {
       first_name: userData.first_name,
       last_name: userData.last_name,
